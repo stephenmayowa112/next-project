862d2187beeb97521f239e4a1437caf91c3571a5
stephenmayowa112 <stephenmayowa112@gmail.com>
2024-09-06 09:01:21 +0100

    FEAT: more edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
