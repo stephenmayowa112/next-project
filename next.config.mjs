d13a2ce84da035ec4ea0d1e9e187a3e0a20ef11f
stephenmayowa112 <stephenmayowa112@gmail.com>
2024-09-06 09:16:42 +0100

    FEAT: addressing build issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { reactStrictMode: true,  
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+
   images: {  
     domains: ['picsum.photos'], }};
 
 export default nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
